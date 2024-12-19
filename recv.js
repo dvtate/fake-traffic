3761ba43158febb6fbf0468f3fda75db5f55648b
Dustin Van Tate Testa <toast27@gmail.com>
2024-12-19 13:00:51 -0600

    special trick for crawlers

--- recv.js
+++ recv.js
@@ -7,6 +7,14 @@ app.post('/secure', async (req, res) => {
     res.send(util.randData(50000));
 });
 
+
+// Send a bunch of garbage to crawlers
+const crawlerRoute = (req, res) => {
+    res.send(util.randData());
+};
+app.get('*', crawlerRoute);
+app.post('*', crawlerRoute);
+
 const port = process.env.PORT || 22121;
 app.listen(port, () => { 
     console.log('now listening on port ', port);
